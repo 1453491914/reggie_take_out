3d7b21982833b95455e2129144838a50eb6ee233
Natsume <1453491914@qq.com>
2023-03-09 10:34:35 +0800

    添加了Swagger依赖，可以生成api文档

--- src/main/java/com/example/reggie_take_out/config/WebMvcConfig.java
+++ src/main/java/com/example/reggie_take_out/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package com.example.reggie_take_out.config;
 
 import com.example.reggie_take_out.common.JacksonObjectMapper;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import interceptor.FrontLoginCheckInterceptor;
 import interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -10,6 +12,13 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -18,20 +27,27 @@ import java.util.List;
  * @version 1.0.0
  */
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     /**
      * 设置静态的资源映射
+     *
      * @param registry
      */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/static/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/static/front/");
+
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     /**
      * 扩展mvc框架的消息转换器
+     *
      * @param converters
      */
     @Override
@@ -41,23 +57,42 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //设置对象转换器，底层用Jackson将Java对象转换成json
         converter.setObjectMapper(new JacksonObjectMapper());
         //添加消息转换器
-        converters.add(0,converter);
+        converters.add(0, converter);
     }
 
     /**
      * 添加拦截器
+     *
      * @param registry
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         InterceptorRegistration loginCheck = registry.addInterceptor(new LoginCheckInterceptor());
-        loginCheck.addPathPatterns("/backend/index.html","employee/**");
+        loginCheck.addPathPatterns("/backend/index.html", "employee/**");
         //loginCheck.excludePathPatterns("/employee/login",
         //        "/employee/logout","/api/**","/images/**","/backend/page/login/login.html","/js/**","plugins/**","styles/**","/favicon.ico");
         InterceptorRegistration frontLoginCheck = registry.addInterceptor(new FrontLoginCheckInterceptor());
-        frontLoginCheck.addPathPatterns("/front/index.html","/front/page/**")
+        frontLoginCheck.addPathPatterns("/front/index.html", "/front/page/**")
                 .excludePathPatterns("/front/page/login.html");
     }
 
+    @Bean
+    public Docket createRestApi() {
+        //文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.reggie_take_out.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version(" 1. 0")
+                .description("瑞吉外安接口文档")
+                .build();
+    }
 
 }
